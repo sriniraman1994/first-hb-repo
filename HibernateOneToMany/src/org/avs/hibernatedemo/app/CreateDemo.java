bb676a3b85585ed2ad4c75cf192e5ddb6aaa3608
admin <admin@admin-PC>
2018-03-13 23:03:00 +0530

    refs #first-hib-repo HibernateOneTomany commit

--- HibernateOneToMany/src/org/avs/hibernatedemo/app/CreateDemo.java
+++ HibernateOneToMany/src/org/avs/hibernatedemo/app/CreateDemo.java
@@ -9,7 +9,11 @@ import org.avs.hibernatedemo.entity.InstructorDetails;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-
+/**
+ * 
+ * @author srinivasan.S
+ *
+ */
 public class CreateDemo {
 public static void main(String[] args) {
 	SessionFactory sf = new Configuration().configure().addAnnotatedClass(Instructor.class).addAnnotatedClass(InstructorDetails.class).addAnnotatedClass(Course.class).buildSessionFactory();
